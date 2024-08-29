0574332991ca591e7addbe2ea422b612dcaeb759
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-08-29 17:32:10 -0300

    inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostsComponent } from './pages/single-posts/single-posts.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'category', component: SingleCategoryComponent},
+    {path: 'post', component: SinglePostsComponent},
+
+    {path: 'about', component: AboutUsComponent},
+    {path: 'term-conditions', component: TermsAndConditionComponent},
+    {path: 'contact', component: ContactUsComponent}
+];
---
+++ src/app/pages/single-posts/single-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-posts.component.html',
+  styleUrl: './single-posts.component.css'
+})
+export class SinglePostsComponent {
+
+}
